18b0047dc893b53d02cd40e96d066816002e8b77
Nethra G S <nethra.2102130@srec.ac.in>
2024-10-19 17:34:29 +0530

    Spring Initalizr

---
+++ BackendAnalytics/src/main/java/com/UserAnalytics/BackendAnalytics/BackendAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.UserAnalytics.BackendAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAnalyticsApplication.class, args);
+	}
+
+}
---
+++ BackendAnalytics/src/test/java/com/UserAnalytics/BackendAnalytics/BackendAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserAnalytics.BackendAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
